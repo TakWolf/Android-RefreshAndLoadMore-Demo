a1e92ff5497522788c229e99f21a54439364087e
TakWolf <takwolf@foxmail.com>
2022-01-27 20:29:44 +0800

    升级构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    }
+plugins {
+    id("com.android.application") version "7.1.0" apply false
+    id("com.android.library") version "7.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("com.google.devtools.ksp") version "1.6.10-1.0.2" apply false
 }
 
 tasks {
