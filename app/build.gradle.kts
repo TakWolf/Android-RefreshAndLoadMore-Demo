f5f4eaeffff64ff67136794936771fc1e796bcaa
TakWolf <takwolf@foxmail.com>
2024-09-01 05:06:46 +0800

    升级构建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil:2.7.0")
 }
