ae073c95a69b2a5d04a60980005a62cfae3fe9b6
TakWolf <takwolf@foxmail.com>
2025-03-27 21:07:02 +0800

    升级依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
