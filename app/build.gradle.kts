6ce95f06ca8425461655345fba624b60b17f025a
TakWolf <takwolf@foxmail.com>
2025-03-30 05:03:05 +0800

    升级 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.demo.refreshandloadmore"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takwolf.android.demo.refreshandloadmore"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.0.1"
     }
